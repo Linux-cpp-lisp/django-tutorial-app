24f63074f40d639e97caed20dd08a67d7d6f267d
A M <bugzillauser11@gmail.com>
2014-08-14 11:27:26 -0600

    Added skeleton and URLs for views

--- tutorialapp/polls/views.py
+++ tutorialapp/polls/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse("Hello world!")
+
+def detail(request, poll_id):
+    return HttpResponse("Details of poll %s" % poll_id)
+
+def results(request, poll_id):
+    return HttpResponse("Results of poll %s" % poll_id)
+
+def vote(request, poll_id):
+    return HttpResponse("Voting on poll %s" % poll_id)
