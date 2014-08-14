6c15f3cb91ea5e549b26b87ab13875f622805891
A M <bugzillauser11@gmail.com>
2014-08-14 11:47:25 -0600

    Added basic detail view

--- tutorialapp/polls/views.py
+++ tutorialapp/polls/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 
 from polls.models import Poll
 
@@ -8,7 +8,8 @@ def index(request):
     return render(request, 'polls/index.html', {'latest_polls' : latest_polls})
 
 def detail(request, poll_id):
-    return HttpResponse("Details of poll %s" % poll_id)
+    poll = get_object_or_404(Poll, pk=poll_id)
+    return render(request, 'polls/detail.html', {'poll' : poll})
 
 def results(request, poll_id):
     return HttpResponse("Results of poll %s" % poll_id)
