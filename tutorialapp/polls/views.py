3ce7e4da6678c98e6f65aee645b5f3c78d6a745e
A M <bugzillauser11@gmail.com>
2014-08-14 12:28:12 -0600

    Refactored to use generic views

--- tutorialapp/polls/views.py
+++ tutorialapp/polls/views.py
@@ -1,20 +1,26 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.views import generic
+from django.utils import timezone
 
 from polls.models import Poll, Choice
 
-def index(request):
-    latest_polls = Poll.objects.order_by("-pub_date")[:5]
-    return render(request, 'polls/index.html', {'latest_polls' : latest_polls})
-
-def detail(request, poll_id):
-    poll = get_object_or_404(Poll, pk=poll_id)
-    return render(request, 'polls/detail.html', {'poll' : poll})
-
-def results(request, poll_id):
-    poll = get_object_or_404(Poll, pk=poll_id)
-    return render(request, 'polls/results.html', {'poll' : poll})
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_polls"
+    
+    def get_queryset(self):
+        """Returns the latest 5 polls"""
+        return Poll.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
+    
+class DetailView(generic.DetailView):
+    model = Poll
+    template_name = 'polls/detail.html'
+    
+class ResultsView(generic.DetailView):
+    model = Poll
+    template_name = 'polls/results.html'
 
 def vote(request, poll_id):
     poll = get_object_or_404(Poll, pk=poll_id)
