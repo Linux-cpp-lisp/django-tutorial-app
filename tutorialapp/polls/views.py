0ff4b6d187d3bb5c76f0c5e420ce7302245c76f0
A M <bugzillauser11@gmail.com>
2014-08-14 12:02:51 -0600

    Added voting

--- tutorialapp/polls/views.py
+++ tutorialapp/polls/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
-from polls.models import Poll
+from polls.models import Poll, Choice
 
 def index(request):
     latest_polls = Poll.objects.order_by("-pub_date")[:5]
@@ -15,4 +16,13 @@ def results(request, poll_id):
     return HttpResponse("Results of poll %s" % poll_id)
 
 def vote(request, poll_id):
-    return HttpResponse("Voting on poll %s" % poll_id)
+    poll = get_object_or_404(Poll, pk=poll_id)
+    try:
+        choice = poll.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {'poll' : poll, 'error_message' : "You must select a choice."})
+    else:
+        choice.votes += 1
+        choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(poll.id,)))
+    
