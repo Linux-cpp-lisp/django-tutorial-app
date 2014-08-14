202373eb984b64546574ba0689c527857d720635
A M <bugzillauser11@gmail.com>
2014-08-14 12:09:59 -0600

    Added results view

--- tutorialapp/polls/views.py
+++ tutorialapp/polls/views.py
@@ -13,7 +13,8 @@ def detail(request, poll_id):
     return render(request, 'polls/detail.html', {'poll' : poll})
 
 def results(request, poll_id):
-    return HttpResponse("Results of poll %s" % poll_id)
+    poll = get_object_or_404(Poll, pk=poll_id)
+    return render(request, 'polls/results.html', {'poll' : poll})
 
 def vote(request, poll_id):
     poll = get_object_or_404(Poll, pk=poll_id)
