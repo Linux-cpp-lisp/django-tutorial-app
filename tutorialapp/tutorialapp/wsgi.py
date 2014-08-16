df33d558beef8ba9228d356f9531ffcccf16b19d
A M <bugzillauser11@gmail.com>
2014-08-16 10:06:17 -0600

    Modified wsgi.py to work with Apache

--- tutorialapp/tutorialapp/wsgi.py
+++ tutorialapp/tutorialapp/wsgi.py
@@ -8,6 +8,9 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+sys.path.insert(0, '/var/www/django-polls/tutorialapp/')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tutorialapp.settings")
 
 from django.core.wsgi import get_wsgi_application
