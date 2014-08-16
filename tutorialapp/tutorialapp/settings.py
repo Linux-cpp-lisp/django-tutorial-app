dd09d14a08cead8e720cacca4b1c54ec22cdf154
A M <bugzillauser11@gmail.com>
2014-08-16 08:25:19 -0600

    Changed to use MySQL database

--- tutorialapp/tutorialapp/settings.py
+++ tutorialapp/tutorialapp/settings.py
@@ -60,8 +60,12 @@ WSGI_APPLICATION = 'tutorialapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE' : 'django.db.backends.mysql',
+        'NAME' :'pollsapp',
+        'USER' : 'polls',
+        'PASSWORD' : 'sqlsucks',
+        'HOST' : 'localhost',
+        'PORT' : '3306'
     }
 }
 
